8c1f28e045be5444c4c293d820fc50edf1730802
suchael <suchael8@gmail.com>
2023-12-16 11:43:47 -0800

    added new buttons

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, ScrollView } from "react-native";
 
-
-import News from './src/News/News.js';
-import Questions from './src/Questions/Questions.js';
-import CreateNews from "./src/News/CreateNews.js"
+import News from "./src/News/News.js";
+import Questions from "./src/Questions/Questions.js";
+import CreateNews from "./src/News/CreateNews.js";
 
 const Stack = createStackNavigator();
 
@@ -27,42 +26,68 @@ export default Navigation;
 
 const Home = ({ navigation }) => {
   const navigateToNews = () => {
-    navigation.navigate('News');
+    navigation.navigate("News");
   };
 
   const navigateToQuestions = () => {
-    navigation.navigate('Questions');
+    navigation.navigate("Questions");
   };
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.button} onPress={navigateToNews}>
-        <Text style={styles.buttonText}>News</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={navigateToQuestions}>
-        <Text style={styles.buttonText}>Questions</Text>
-      </TouchableOpacity>
+      <View style={styles.row1}>
+        <TouchableOpacity style={styles.button} onPress={navigateToQuestions}>
+          <Text style={styles.buttonText}>Questions</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={navigateToNews}>
+          <Text style={styles.buttonText}>News</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.row2}>
+        <TouchableOpacity style={styles.button} onPress={navigateToNews}>
+          <Text style={styles.buttonText}>Teachers Payment</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={navigateToQuestions}>
+          <Text style={styles.buttonText}>Students Payment</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 20,
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    borderWidth: 2,
+    borderColor: "lightgray",
+    margin: 5,
+    paddingVertical: 20,
+  },
+  row1: {
+    flex: 1,
+    justifyContent: "space-around",
+    // alignItems: 'center',
+    flexDirection: "row",
+  },
+  row2: {
+    flex: 1,
+    justifyContent: "space-around",
+    // alignItems: 'center',
+    flexDirection: "row",
   },
   button: {
-    backgroundColor: 'blue', 
+    backgroundColor: "blue",
     margin: 10,
     borderRadius: 10,
-    height: 40,
+    height: 50,
     width: 120,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   buttonText: {
-    color: 'white',
+    color: "white",
     fontSize: 16,
   },
 });
